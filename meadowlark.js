f6ec774a41361f13009dc7e3a5a6303ab1c1b6ec
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-12-07 22:46:50 -0500

    form handling with Express

--- meadowlark.js
+++ meadowlark.js
@@ -1,36 +1,52 @@
+// Import required modules
 const express = require('express');
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars').create({
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    },
+  },
+});
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
+const handlers = require('./lib/handlers');
+const weatherMiddleware = require('./lib/middleware/weather');
 
+// Create an instance of the Express application
 const app = express();
 
-// Create an instance of express-handlebars
-const hbs = exphbs.create();
-
 // Configure Handlebars view engine
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', exphbs.engine);
 app.set('view engine', 'handlebars');
 
+// Use body-parser middleware for parsing URL-encoded and JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static files from the 'public' directory
 app.use(express.static(__dirname + '/public'));
 
+// Use custom weatherMiddleware
+app.use(weatherMiddleware);
+
+// Define routes
+app.get('/', handlers.home);
+// ... other route handlers ...
+
+// Error handling middleware
+app.use(handlers.notFound);
+app.use(handlers.serverError);
+
+// Start the server
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home.handlebars'));
-
-app.get('/about', (req, res) => res.render('about.handlebars'));
-
-// Custom 404 page
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
-
-// Custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render('500');
-});
-
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`));
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`);
+  });
+} else {
+  module.exports = app;
+}
